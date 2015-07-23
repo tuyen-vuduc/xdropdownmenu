e256823b5fd595a8ab1e28670941e4b513a910ff
Vu Duc Tuyen <kan.vuduc@gmail.com>
2015-07-23 09:55:42 +0700

    Add sample table view source

--- T.NavigationDropdownMenu/ViewController.cs
+++ T.NavigationDropdownMenu/ViewController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 
 using UIKit;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace T.NavigationDropdownMenu
 {
-	public partial class ViewController : UIViewController
+	public partial class ViewController : UITableViewController
 	{
 		public ViewController (IntPtr handle) : base (handle)
 		{
@@ -14,6 +16,13 @@ namespace T.NavigationDropdownMenu
 		{
 			base.ViewDidLoad ();
 			// Perform any additional setup after loading the view, typically from a nib.
+
+			var items = new List<string> ();
+			for (int i = 0; i < 50; i++) {
+				items.Add ("NavigationDropdownMenu " + i);
+			}
+
+			TableView.Source = new SampleSource (items);
 		}
 
 		public override void DidReceiveMemoryWarning ()
@@ -21,6 +30,36 @@ namespace T.NavigationDropdownMenu
 			base.DidReceiveMemoryWarning ();
 			// Release any cached data, images, etc that aren't in use.
 		}
+	}
+
+	public class SampleSource : UITableViewSource {
+
+		private readonly IEnumerable<string> items;
+
+		public SampleSource (IEnumerable<string> items)
+		{
+			this.items = items;
+		}
+
+		#region implemented abstract members of UITableViewSource
+		public override UITableViewCell GetCell (UITableView tableView, Foundation.NSIndexPath indexPath)
+		{
+			var cell = tableView.DequeueReusableCell ("sample");
+
+			if (cell == null) {
+				cell = new UITableViewCell (UITableViewCellStyle.Value1, "sample");
+			}
+
+			cell.TextLabel.Text = items.ElementAt (indexPath.Row);
+
+			return cell;
+		}
+		public override nint RowsInSection (UITableView tableview, nint section)
+		{
+			return (nint) items.Count ();
+		}
+		#endregion
+		
 	}
 }
 
